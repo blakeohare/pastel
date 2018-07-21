f3a2f75df6b76aee35c8e54bb7585bcba649f45d
Blake O'Hare <mspaintmaestro@gmail.com>
2018-07-22 00:33:37 +0900

    Use a JSON file to define properties of each transpiler project. Generate output for all languages in bulk
    Delete old extraneous file.

--- Source/Program.cs
+++ Source/Program.cs
@@ -12,44 +12,55 @@ namespace Pastel
 
 #if DEBUG
             effectiveArgs = new string[] {
-                @"C:\Things\Pastel\Samples\ListAnalyzer\pastel",
-                @"C:\Things\Pastel\Samples\ListAnalyzer\java\src\gen",
-                "java"
+                @"C:\Things\Pastel\Samples\ListAnalyzer\ListAnalyzer.json",
             };
 #endif
 
-            string sourceDir = effectiveArgs[0];
-            string targetDir = effectiveArgs[1];
-            string platform = effectiveArgs[2];
+            string manifestPath = System.IO.Path.Combine(System.IO.Directory.GetCurrentDirectory(), effectiveArgs[0]);
+            manifestPath = System.IO.Path.GetFullPath(manifestPath);
+            string projectRoot = System.IO.Path.GetDirectoryName(manifestPath);
 
-            Language lang;
-            switch (platform)
+            SimpleJson.JsonParser manifestJson = new SimpleJson.JsonParser(System.IO.File.ReadAllText(manifestPath));
+            manifestJson.Parse();
+            IDictionary<string, object> manifestRoot = manifestJson.ParseAsDictionary();
+            SimpleJson.JsonLookup jl = new SimpleJson.JsonLookup(manifestRoot);
+            string sourceDir = System.IO.Path.GetFullPath(System.IO.Path.Combine(projectRoot, jl.GetAsString("source")));
+            object[] targets = jl.GetAsList("targets");
+            for (int i = 0; i < targets.Length; ++i)
             {
-                case "csharp": lang = Language.CSHARP; break;
-                case "java": lang = Language.JAVA; break;
-                case "python": lang = Language.PYTHON; break;
-                default: throw new Exception();
-            }
+                SimpleJson.JsonLookup target = new SimpleJson.JsonLookup((IDictionary<string, object>)targets[i]);
 
-            Dictionary<string, string> files = new FileGatherer(sourceDir, ".pst").GatherFiles();
-            Parser parser = new Parser(new Dictionary<string, object>(), new InlineImportCodeLoader());
-            List<ParseNodes.ICompilationEntity> nodes = new List<ParseNodes.ICompilationEntity>();
-            PastelCompiler pc = new PastelCompiler(lang, new PastelCompiler[0], new Dictionary<string, object>(), new InlineImportCodeLoader());
-            foreach (string filename in files.Keys.OrderBy(k => k))
-            {
-                pc.CompileBlobOfCode(filename, files[filename]);
-                pc.Resolve();
-            }
+                string platform = target.GetAsString("platform");
+                string targetDir = System.IO.Path.GetFullPath(System.IO.Path.Combine(projectRoot, target.GetAsString("output")));
 
-            Transpilers.AbstractTranspiler transpiler = LanguageUtil.GetTranspiler(lang);
-            Dictionary<string, string> fileOutput = new Dictionary<string, string>();
-            Transpilers.TranspilerContext transpilerContext = new Transpilers.TranspilerContext(lang);
-            transpiler.GenerateCode(transpilerContext, pc, fileOutput);
-            foreach (string filename in fileOutput.Keys)
-            {
-                string absolutePath = System.IO.Path.Combine(targetDir, filename);
-                string code = fileOutput[filename];
-                Util.WriteFileText(absolutePath, code);
+                Language lang;
+                switch (platform)
+                {
+                    case "csharp": lang = Language.CSHARP; break;
+                    case "java": lang = Language.JAVA; break;
+                    case "python": lang = Language.PYTHON; break;
+                    default: throw new Exception();
+                }
+
+                Dictionary<string, string> files = new FileGatherer(sourceDir, ".pst").GatherFiles();
+                List<ParseNodes.ICompilationEntity> nodes = new List<ParseNodes.ICompilationEntity>();
+                PastelCompiler pc = new PastelCompiler(lang, new PastelCompiler[0], new Dictionary<string, object>(), new InlineImportCodeLoader());
+                foreach (string filename in files.Keys.OrderBy(k => k))
+                {
+                    pc.CompileBlobOfCode(filename, files[filename]);
+                    pc.Resolve();
+                }
+
+                Transpilers.AbstractTranspiler transpiler = LanguageUtil.GetTranspiler(lang);
+                Dictionary<string, string> fileOutput = new Dictionary<string, string>();
+                Transpilers.TranspilerContext transpilerContext = new Transpilers.TranspilerContext(lang);
+                transpiler.GenerateCode(transpilerContext, pc, fileOutput);
+                foreach (string filename in fileOutput.Keys)
+                {
+                    string absolutePath = System.IO.Path.Combine(targetDir, filename);
+                    string code = fileOutput[filename];
+                    Util.WriteFileText(absolutePath, code);
+                }
             }
         }
     }
