53d5e1117ddd310386e400eddde895abe114b3a9
Blake O'Hare <mspaintmaestro@gmail.com>
2018-07-21 22:19:42 +0900

    Stub out a sample C# project for ListAnalyzer

---
+++ Samples/ListAnalyzer/csharp/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+
+namespace ListAnalyzer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                int[] nums = args.Select(n => int.Parse(n)).ToArray();
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Usage: ListAnalyzer.exe num1 num2 num3 ...");
+                return;
+            }
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
