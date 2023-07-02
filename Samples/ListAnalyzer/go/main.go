34c54a5305078571d8eb8b77918edd1037fb3155
Blake O'Hare <mspaintmaestro@gmail.com>
2023-07-02 14:59:44 +0900

    List analyzer sample now successfully transpiles to GoLang

--- Samples/ListAnalyzer/go/main.go
+++ Samples/ListAnalyzer/go/main.go
@@ -4,4 +4,14 @@ import "fmt"
 
 func main() {
 	fmt.Println("Hello, World!")
+
+	nums := []int{5, 9, 2, 4, 11, 13}
+	result := fn_perform_analysis(nums, len(nums)).o
+
+	fmt.Println("Count:", result.f_count)
+	fmt.Println("Min:", result.f_min)
+	fmt.Println("Max:", result.f_max)
+	fmt.Println("Mean:", result.f_mean)
+	fmt.Println("Median:", result.f_median)
+	fmt.Println("Standard Deviation:", result.f_std_dev)
 }
