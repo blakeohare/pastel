e10e81a3d3edebb2ce771b608a33ebd4b55269d7
Blake O'Hare <mspaintmaestro@gmail.com>
2023-07-02 11:59:01 +0900

    Update to .NET Core 7.0 but
    This introduces PastelLegacy.sln/csproj which still points to .NET Core 3.1 as I am unfortunately still restricted by some unupgradeable hardware at the moment.

--- Release/release.py
+++ Release/release.py
@@ -19,15 +19,25 @@ def prepareCleanDirectory(d):
 def main(args):
 
 	if len(args) != 1:
-		print("usage: python release.py windows|mac|linux")
+		print("usage: python release.py platform")
+		print("platform options:")
+		print("  windows")
+		print("  mac")
+		print("  linux")
+		print("  win31 <-- NOT Windows 3.1. Windows using .NET Core 3.1")
 		return
-	
+
 	isWindows = False
 	isMac = False
 	isLinux = False
 
 	dotnet_platform = ''
 	platform_name = args[0]
+	use_dotnet_core_31 = False
+	if args[0] == 'win31':
+		args[0] = 'windows'
+		use_dotnet_core_31 = True
+
 	if args[0] == 'windows':
 		isWindows = True
 		dotnet_platform = 'win-x64'
@@ -43,27 +53,29 @@ def main(args):
 	else:
 		print("Invalid platform: " + args[0])
 		return
-	
+
 	copy_to_dir = 'pastel-' + VERSION + '-' + platform_name
 	executable_path = ''
+	net_dir = 'netcoreapp3.1' if use_dotnet_core_31 else 'net7.0'
+	sln_name = 'PastelLegacy.sln' if use_dotnet_core_31 else 'Pastel.sln'
 	if isWindows:
-		executable_dir = os.path.join('..', 'Source', 'bin', 'Release', 'netcoreapp3.1', dotnet_platform, 'publish')
+		executable_dir = os.path.join('..', 'Source', 'bin', 'Release', net_dir, dotnet_platform, 'publish')
 		executable_path = os.path.join(executable_dir, 'Pastel.exe')
 	elif isMac:
-		executable_dir = os.path.join('..', 'Source', 'bin', 'Release', 'netcoreapp3.1', dotnet_platform, 'publish')
+		executable_dir = os.path.join('..', 'Source', 'bin', 'Release', net_dir, dotnet_platform, 'publish')
 		executable_path = os.path.join(executable_dir, 'Pastel')
 	else:
 		print("This hasn't been tested yet.")
 		return
-	
+
 	prepareCleanDirectory(copy_to_dir)
-	
+
 	if os.path.exists(executable_dir):
 		shutil.rmtree(executable_dir)
-	
+
 	cmd = ' '.join([
 		'dotnet publish',
-		os.path.join('..', 'Source', 'Pastel.sln'),
+		os.path.join('..', 'Source', sln_name),
 		'-c Release',
 		'-r', dotnet_platform,
 		'--self-contained true',
@@ -71,9 +83,11 @@ def main(args):
 		'-p:PublishSingleFile=true'
 	])
 	result = runCommand(cmd)
-	
-	shutil.copyfile(executable_path, os.path.join(copy_to_dir, final_exec_name))
-	
+
+	dest_exec_path = os.path.join(copy_to_dir, final_exec_name)
+	shutil.copyfile(executable_path, dest_exec_path)
+	if isMac:
+		runCommand('chmod 0777 ' + dest_exec_path)
 	print("Release directory created: " + copy_to_dir)
 
 main(sys.argv[1:])
