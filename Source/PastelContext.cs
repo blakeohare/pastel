831cc218123a35b1ec842a8ba60f2f36540666f6
Blake O'Hare <mspaintmaestro@gmail.com>
2024-11-13 09:24:56 +0900

    Move the code that injects the transpilation helper functions into the user's function code into its own well-defined utility class.
    This is actually larger than the PastelContext class itself and was obscuring more important codepath logic.

--- Source/PastelContext.cs
+++ Source/PastelContext.cs
@@ -8,7 +8,7 @@ namespace Pastel
 {
     public class PastelContext
     {
-        private PastelCompiler lazyInitCompiler = null;
+        private PastelCompiler? lazyInitCompiler = null;
         public Language Language { get; private set; }
         private Dictionary<string, object> constants = new Dictionary<string, object>();
         internal ExtensionSet ExtensionSet { get; private set; }
@@ -96,7 +96,7 @@ namespace Pastel
         public Dictionary<string, string> GetCodeForClasses()
         {
             TranspilerContext ctx = this.TranspilerContext;
-            Dictionary<string, string> output = new Dictionary<string, string>();
+            Dictionary<string, string> output = [];
             foreach (ClassDefinition cd in this.GetCompiler().GetClassDefinitions())
             {
                 this.Transpiler.GenerateCodeForClass(ctx, cd);
@@ -108,7 +108,7 @@ namespace Pastel
         public Dictionary<string, string> GetCodeForStructs()
         {
             TranspilerContext ctx = this.TranspilerContext;
-            Dictionary<string, string> output = new Dictionary<string, string>();
+            Dictionary<string, string> output = [];
             foreach (StructDefinition sd in this.GetCompiler().GetStructDefinitions())
             {
                 this.Transpiler.GenerateCodeForStruct(ctx, sd);
@@ -138,172 +138,13 @@ namespace Pastel
 
         public string GetCodeForFunctions()
         {
-            TranspilerContext ctx = this.TranspilerContext;
             Dictionary<string, string> output = this.GetCodeForFunctionsLookup();
 
-            System.Text.StringBuilder userCodeBuilder = new System.Text.StringBuilder();
+            string userCode = string.Join("\n\n", output.Keys.OrderBy(name => name).Select(name => output[name]));
+            string userCodeWithPastelHelpers = TranspilationHelperCodeUtil.InjectTranspilationHelpers(
+                this.TranspilerContext.Transpiler, userCode);
 
-            foreach (string fnName in output.Keys.OrderBy(s => s))
-            {
-                userCodeBuilder.Append("\n\n");
-                userCodeBuilder.Append(output[fnName]);
-            }
-
-            string userCode = userCodeBuilder.ToString().Trim();
-
-            Dictionary<string, string> codeChunks = this.GetCodeChunks(ctx.Transpiler, userCode);
-            string[] chunkOrder = this.GetChunkOrder(codeChunks, userCode);
-
-            List<string> finalCodeBuilder = new List<string>();
-            foreach (string chunkId in chunkOrder)
-            {
-                finalCodeBuilder.Add(codeChunks[chunkId]);
-            }
-
-            string finalCode = string.Join("\n\n", finalCodeBuilder);
-            return finalCode;
-        }
-
-        private Dictionary<string, string> GetCodeChunks(AbstractTranspiler transpiler, string userCode)
-        {
-            string resourcePath = transpiler.HelperCodeResourcePath;
-            Dictionary<string, string> output = new Dictionary<string, string>();
-            if (resourcePath == null) return output;
-
-            string helperCode = ResourceReader.ReadTextFile(resourcePath);
-
-            string currentId = null;
-            List<string> currentChunk = new List<string>();
-            foreach (string lineRaw in helperCode.Split('\n'))
-            {
-                string line = lineRaw.TrimEnd();
-                if (line.Contains("PASTEL_ENTITY_ID"))
-                {
-                    if (currentId != null)
-                    {
-                        output[currentId] = string.Join("\n", currentChunk).Trim();
-                    }
-                    currentId = line.Split(':')[1].Trim();
-                    currentChunk.Clear();
-                }
-                else
-                {
-                    currentChunk.Add(lineRaw);
-                }
-            }
-
-            if (currentId != null)
-            {
-                output[currentId] = string.Join("\n", currentChunk).Trim();
-            }
-
-            output[""] = userCode;
-
-            return output;
-        }
-
-
-        private string[] GetChunkOrder(Dictionary<string, string> chunksByMarker, string userCode)
-        {
-            HashSet<string> allMarkers = new HashSet<string>(chunksByMarker.Keys);
-            List<string> nonPstPrefixThings = new List<string>();
-            List<string> pstPrefixedThings = new List<string>();
-            foreach (string marker in chunksByMarker.Keys.OrderBy(m => m).Where(m => m.Length > 0))
-            {
-                if (marker.StartsWith("PST"))
-                {
-                    pstPrefixedThings.Add(marker);
-                }
-                else
-                {
-                    nonPstPrefixThings.Add(marker);
-                }
-            }
-            Dictionary<string, string[]> dependencies = new Dictionary<string, string[]>();
-            foreach (string marker in chunksByMarker.Keys.OrderBy(m => m))
-            {
-                string code = chunksByMarker[marker];
-                dependencies[marker] = FindUsedMarkers(code, nonPstPrefixThings, pstPrefixedThings, allMarkers, marker).ToArray();
-            }
-
-            dependencies["PST_RegisterExtensibleCallback"] = new string[] { "PST_ExtCallbacks" };
-            dependencies[""] = dependencies[""].Concat(new string[] { "PST_RegisterExtensibleCallback" }).ToArray();
-
-            List<string> orderedKeys = new List<string>();
-
-            this.PopulateOrderedChunkKeys("", orderedKeys, dependencies, new Dictionary<string, int>());
-
-            return orderedKeys.ToArray();
-        }
-
-        private void PopulateOrderedChunkKeys(
-            string currentItem,
-            List<string> orderedKeys,
-            Dictionary<string, string[]> dependencies,
-            Dictionary<string, int> traversalState) // { missing/0 - not used | 1 - seen but dependencies not added yet | 2 - added along with all dependencies }
-        {
-            if (!traversalState.ContainsKey(currentItem))
-            {
-                traversalState[currentItem] = 1;
-            }
-            else if (traversalState[currentItem] == 1)
-            {
-                throw new System.Exception("dependency loop: " + currentItem + " depends on itself indirectly.");
-            }
-            else if (traversalState[currentItem] == 2)
-            {
-                return; // already added
-            }
-
-            foreach (string dep in dependencies[currentItem])
-            {
-                PopulateOrderedChunkKeys(dep, orderedKeys, dependencies, traversalState);
-            }
-
-            traversalState[currentItem] = 2;
-            orderedKeys.Add(currentItem);
-        }
-
-        private IList<string> FindUsedMarkers(string code, IList<string> nonPstMarkers, IList<string> pstMarkers, HashSet<string> allMarkers, string exclusion)
-        {
-            List<string> usedMarkers = new List<string>();
-            foreach (string nonPstMarker in nonPstMarkers)
-            {
-                if (exclusion != nonPstMarker && code.Contains(nonPstMarker))
-                {
-                    usedMarkers.Add(nonPstMarker);
-                }
-            }
-
-            string[] pstParts = code.Split(new string[] { "PST" }, System.StringSplitOptions.None);
-            for (int i = 1; i < pstParts.Length; ++i)
-            {
-                string markerName = GetMarkerNameHacky(pstParts[i]);
-                if (allMarkers.Contains(markerName) && exclusion != markerName)
-                {
-                    usedMarkers.Add(markerName);
-                }
-            }
-
-            return usedMarkers;
-        }
-
-        private string GetMarkerNameHacky(string potentialMarkerNameWithoutPst)
-        {
-            char c;
-            for (int i = 0; i < potentialMarkerNameWithoutPst.Length; ++i)
-            {
-                c = potentialMarkerNameWithoutPst[i];
-                if (!((c >= 'a' && c <= 'z') ||
-                    (c >= 'A' && c <= 'Z') ||
-                    (c >= '0' && c <= '9') ||
-                    c == '_' ||
-                    c == '$'))
-                {
-                    return "PST" + potentialMarkerNameWithoutPst.Substring(0, i);
-                }
-            }
-            return "PST" + potentialMarkerNameWithoutPst;
+            return userCodeWithPastelHelpers;
         }
     }
 }
---
+++ Source/TranspilationHelperCodeUtil.cs
@@ -0,0 +1,170 @@
+﻿using Pastel.Transpilers;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Pastel
+{
+    internal static class TranspilationHelperCodeUtil
+    {
+        public static string InjectTranspilationHelpers(AbstractTranspiler transpiler, string userCode)
+        {
+            Dictionary<string, string> codeChunks = GetCodeChunks(transpiler, userCode);
+            string[] chunkOrder = GetChunkOrder(codeChunks, userCode);
+
+            List<string> finalCodeBuilder = [];
+            foreach (string chunkId in chunkOrder)
+            {
+                finalCodeBuilder.Add(codeChunks[chunkId]);
+            }
+
+            string finalCode = string.Join("\n\n", finalCodeBuilder);
+            return finalCode;
+        }
+
+        private static Dictionary<string, string> GetCodeChunks(AbstractTranspiler transpiler, string userCode)
+        {
+            string resourcePath = transpiler.HelperCodeResourcePath;
+            Dictionary<string, string> output = [];
+            if (resourcePath == null) return output;
+
+            string helperCode = ResourceReader.ReadTextFile(resourcePath);
+
+            string? currentId = null;
+            List<string> currentChunk = [];
+            foreach (string lineRaw in helperCode.Split('\n'))
+            {
+                string line = lineRaw.TrimEnd();
+                if (line.Contains("PASTEL_ENTITY_ID"))
+                {
+                    if (currentId != null)
+                    {
+                        output[currentId] = string.Join("\n", currentChunk).Trim();
+                    }
+                    currentId = line.Split(':')[1].Trim();
+                    currentChunk.Clear();
+                }
+                else
+                {
+                    currentChunk.Add(lineRaw);
+                }
+            }
+
+            if (currentId != null)
+            {
+                output[currentId] = string.Join("\n", currentChunk).Trim();
+            }
+
+            output[""] = userCode;
+
+            return output;
+        }
+
+        private static string[] GetChunkOrder(Dictionary<string, string> chunksByMarker, string userCode)
+        {
+            HashSet<string> allMarkers = [.. chunksByMarker.Keys];
+            List<string> nonPstPrefixThings = [];
+            List<string> pstPrefixedThings = [];
+            foreach (string marker in chunksByMarker.Keys.Where(m => m.Length > 0).OrderBy(m => m))
+            {
+                if (marker.StartsWith("PST"))
+                {
+                    pstPrefixedThings.Add(marker);
+                }
+                else
+                {
+                    nonPstPrefixThings.Add(marker);
+                }
+            }
+            Dictionary<string, string[]> dependencies = [];
+            foreach (string marker in chunksByMarker.Keys.OrderBy(m => m))
+            {
+                string code = chunksByMarker[marker];
+                dependencies[marker] = FindUsedMarkers(code, nonPstPrefixThings, pstPrefixedThings, allMarkers, marker);
+            }
+
+            dependencies["PST_RegisterExtensibleCallback"] = ["PST_ExtCallbacks"];
+            dependencies[""] = [.. dependencies[""], "PST_RegisterExtensibleCallback"];
+
+            List<string> orderedKeys = [];
+
+            PopulateOrderedChunkKeys("", orderedKeys, dependencies, new Dictionary<string, int>());
+
+            return [.. orderedKeys];
+        }
+
+        private static void PopulateOrderedChunkKeys(
+            string currentItem,
+            List<string> orderedKeys,
+            Dictionary<string, string[]> dependencies,
+            Dictionary<string, int> traversalState) // { missing/0 - not used | 1 - seen but dependencies not added yet | 2 - added along with all dependencies }
+        {
+            if (!traversalState.ContainsKey(currentItem))
+            {
+                traversalState[currentItem] = 1;
+            }
+            else if (traversalState[currentItem] == 1)
+            {
+                throw new System.InvalidOperationException("Dependency loop: " + currentItem + " depends on itself indirectly.");
+            }
+            else if (traversalState[currentItem] == 2)
+            {
+                return; // already added
+            }
+
+            foreach (string dep in dependencies[currentItem])
+            {
+                PopulateOrderedChunkKeys(dep, orderedKeys, dependencies, traversalState);
+            }
+
+            traversalState[currentItem] = 2;
+            orderedKeys.Add(currentItem);
+        }
+
+        private static string[] FindUsedMarkers(
+            string code, 
+            IList<string> nonPstMarkers, 
+            IList<string> pstMarkers, 
+            HashSet<string> allMarkers, 
+            string exclusion)
+        {
+            List<string> usedMarkers = [];
+            foreach (string nonPstMarker in nonPstMarkers)
+            {
+                if (exclusion != nonPstMarker && code.Contains(nonPstMarker))
+                {
+                    usedMarkers.Add(nonPstMarker);
+                }
+            }
+
+            string[] pstParts = code.Split("PST");
+            for (int i = 1; i < pstParts.Length; ++i)
+            {
+                string markerName = GetMarkerNameHacky(pstParts[i]);
+                if (allMarkers.Contains(markerName) && exclusion != markerName)
+                {
+                    usedMarkers.Add(markerName);
+                }
+            }
+
+            return [.. usedMarkers];
+        }
+
+        private static string GetMarkerNameHacky(string potentialMarkerNameWithoutPst)
+        {
+            char c;
+            for (int i = 0; i < potentialMarkerNameWithoutPst.Length; ++i)
+            {
+                c = potentialMarkerNameWithoutPst[i];
+                if (!((c >= 'a' && c <= 'z') ||
+                    (c >= 'A' && c <= 'Z') ||
+                    (c >= '0' && c <= '9') ||
+                    c == '_' ||
+                    c == '$'))
+                {
+                    return "PST" + potentialMarkerNameWithoutPst.Substring(0, i);
+                }
+            }
+            return "PST" + potentialMarkerNameWithoutPst;
+        }
+    }
+}
