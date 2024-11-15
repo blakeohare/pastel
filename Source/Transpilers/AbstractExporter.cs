bd7fc77a69accf37c35be16dff7181f5f8adadcd
Blake O'Hare <mspaintmaestro@gmail.com>
2024-11-15 14:06:16 +0900

    Exporter should not use project config denoted directories until final file-write pass. Use stand-in values like `@STRUCT_DIR`

--- Source/Transpilers/AbstractExporter.cs
+++ Source/Transpilers/AbstractExporter.cs
@@ -8,12 +8,12 @@ namespace Pastel.Transpilers
     {
         protected Dictionary<string, string> GenerateFiles(ProjectConfig config, PastelContext context)
         {
-            Dictionary<string, string> files = new Dictionary<string, string>();
+            Dictionary<string, string> files = [];
 
             if (context.UsesStructDefinitions)
             {
                 Dictionary<string, string> structDefinitions = context.GetCodeForStructs();
-                string[] structOrder = structDefinitions.Keys.OrderBy(k => k.ToLower()).ToArray();
+                string[] structOrder = [.. structDefinitions.Keys.OrderBy(k => k.ToLower())];
                 if (context.HasStructsInSeparateFiles)
                 {
                     foreach (string structName in structOrder)
@@ -52,7 +52,11 @@ namespace Pastel.Transpilers
 
             foreach (string path in files.Keys)
             {
-                System.IO.File.WriteAllText(path, files[path]);
+                string actualPath = path
+                    .Replace("@FUNC_FILE", config.OutputFileFunctions)
+                    .Replace("@STRUCT_DIR", config.OutputDirStructs)
+                    .Replace('/', System.IO.Path.DirectorySeparatorChar);
+                System.IO.File.WriteAllText(actualPath, files[path]);
             }
         }
 
@@ -63,37 +67,39 @@ namespace Pastel.Transpilers
             funcCode = transpiler.WrapCodeForFunctions(ctx.TranspilerContext, config, funcCode);
             funcCode = transpiler.WrapFinalExportedCode(funcCode, ctx.GetCompiler().GetFunctionDefinitions());
             funcCode = CodeUtil.ConvertWhitespaceFromCanonicalFormToPreferred(funcCode, transpiler);
-            filesOut[config.OutputFileFunctions] = funcCode;
+            filesOut["@FUNC_FILE"] = funcCode;
         }
 
         private void GenerateStructImplementation(Dictionary<string, string> filesOut, PastelContext ctx, ProjectConfig config, string structName, string structCode)
         {
             structCode = ctx.Transpiler.WrapCodeForStructs(ctx.TranspilerContext, config, structCode);
             string fileExtension = LanguageUtil.GetFileExtension(config.Language);
-            string path = System.IO.Path.Combine(config.OutputDirStructs, structName + fileExtension);
+            string path = "@STRUCT_DIR/" + structName + fileExtension;
             structCode = CodeUtil.ConvertWhitespaceFromCanonicalFormToPreferred(structCode, ctx.Transpiler);
             filesOut[path] = structCode;
         }
 
         private void GenerateStructBundleImplementation(Dictionary<string, string> filesOut, TranspilerContext ctx, ProjectConfig config, string[] structOrder, Dictionary<string, string> structCodeByName)
         {
-            List<string> codeLines = new List<string>();
+            List<string> codeLines = [];
             foreach (string structName in structOrder)
             {
                 codeLines.Add(ctx.Transpiler.WrapCodeForStructs(ctx, config, structCodeByName[structName]));
             }
-            string dir = config.OutputDirStructs;
             string path;
             switch (config.Language)
             {
                 case Language.PHP:
-                    codeLines.Insert(0, "<?php");
-                    codeLines.Add("?>");
-                    path = System.IO.Path.Combine(dir, "gen_classes.php");
+                    codeLines = [
+                        "<?php",
+                        ..codeLines,
+                        "?>",
+                    ];
+                    path = "@STRUCT_DIR/gen_classes.php";
                     break;
 
                 case Language.GO:
-                    path = System.IO.Path.Combine(dir, "genstructs.go");
+                    path = "@STRUCT_DIR/genstructs.go";
                     break;
 
                 default:
