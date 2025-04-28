d8a96d593395070c0f86088286247984e0a5a434
Blake O'Hare <mspaintmaestro@gmail.com>
2025-04-28 10:45:48 +0900

    Add test harness with negative tests

---
+++ testrunner.py
@@ -0,0 +1,99 @@
+import json
+import os
+import random
+import sys
+
+def file_read_text(path):
+    c = open(path, 'rt')
+    t = c.read().replace('\r\n', '\n')
+    c.close()
+    return t
+
+def file_write_text(path, content):
+    c = open(path, 'wb')
+    c.write(content.encode('utf-8'))
+    c.close()
+
+def run_command(ex, args = None):
+    cmd = ex
+    if args != None: cmd += ' ' + ' '.join(args)
+    c = os.popen(cmd)
+    t = c.read()
+    c.close()
+    return t
+
+def run_error_tests(pastel_exec_path):
+    error_dir = os.path.join('tests', 'errors')
+    test_ids = [f[:-len('.out.txt')] for f in os.listdir(error_dir) if f.lower().endswith('.out.txt')]
+    for test_id in test_ids:
+        dst_path = get_temp_dir(test_id)
+        code = file_read_text(os.path.join(error_dir, test_id + '.pst'))
+        code_path = os.path.abspath(os.path.join(dst_path, 'test.pst'))
+        expected = file_read_text(os.path.join(error_dir, test_id + '.out.txt')).strip().replace('\r\n', '\n')
+        lang_id = 'js'
+        if test_id.endswith(']'):
+            lang_id = test_id.split('[').pop()[:-1]
+        build_file = {
+            'source': 'test.pst',
+            'targets': [
+                {
+                    'js': { 'name': 'test', 'language': 'javascript', 'output': { 'functions-path': 'gen.js' } },
+                    'php': { 'name': "php", "language": "php", "output": { "structs-path": "structs", "namespace": "PTest", "functions-path": "gen_functions.php", "functions-wrapper-class": "FunctionWrapper" } }
+                }[lang_id]
+            ]
+        }
+        build_path = os.path.join(dst_path, 'test.json')
+        file_write_text(
+            build_path,
+            json.dumps(build_file, indent = 2))
+        file_write_text(
+            code_path,
+            code)
+
+        actual = run_command(pastel_exec_path, [build_path, 'test']).strip().replace('\r\n', '\n')
+        actual = actual.replace(code_path, 'test.pst')
+        print("Running: " + test_id)
+        if expected != actual:
+            print("FAIL!")
+            print("BUILD FILE:")
+            print("  " + os.path.abspath(build_path))
+            print('-' * 40)
+            print("Expected:\n" + expected)
+            print('-' * 40)
+            print("Actual:\n" + actual)
+            print('-' * 40)
+        else:
+            pass # TODO: delete the tmp directory
+
+LETTERS = 'abcdefghijklmnopqrstuvwyxz'
+CHARS = list(LETTERS + LETTERS.upper() + '0123456789')
+def gen_gibberish(sz):
+    sb = []
+    while len(sb) < sz:
+        sb.append(random.choice(CHARS))
+    return ''.join(sb)
+
+def ensure_dir_exists(path):
+    os.makedirs(path, exist_ok = True)
+
+def get_temp_dir(hint = None):
+    name = gen_gibberish(10)
+    if hint != None: name = hint + '_' + name
+    path = os.path.join('tests', 'tmp', name)
+    ensure_dir_exists(path)
+    return path
+
+def main(args):
+    if len(args) == 0:
+        print("Usage: python testrunner.py path/to/pastel/binary/pastel[.exe]")
+        return
+
+    pastel_path = args[0]
+    if not os.path.exists(pastel_path):
+        print("Path does not exist: " + pastel_path[:999])
+        return
+
+    run_error_tests(pastel_path)
+
+if __name__ == '__main__':
+    main(sys.argv[1:])
